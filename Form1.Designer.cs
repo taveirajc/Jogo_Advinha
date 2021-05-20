4d39cbec841eb79d6fa0d6ca1932a08b0985a838
taveirajc <taveirajc@gmail.com>
2021-05-20 19:13:13 -0300

    Advinha

---
+++ Form1.Designer.cs
@@ -0,0 +1,120 @@
+﻿namespace Advinha_numero
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Variável de designer necessária.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpar os recursos que estão sendo usados.
+        /// </summary>
+        /// <param name="disposing">true se for necessário descartar os recursos gerenciados; caso contrário, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código gerado pelo Windows Form Designer
+
+        /// <summary>
+        /// Método necessário para suporte ao Designer - não modifique 
+        /// o conteúdo deste método com o editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.btn_jogar = new System.Windows.Forms.Button();
+            this.btn_sair = new System.Windows.Forms.Button();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // btn_jogar
+            // 
+            this.btn_jogar.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_jogar.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_jogar.Location = new System.Drawing.Point(126, 196);
+            this.btn_jogar.Name = "btn_jogar";
+            this.btn_jogar.Size = new System.Drawing.Size(90, 39);
+            this.btn_jogar.TabIndex = 0;
+            this.btn_jogar.Text = "Iniciar";
+            this.btn_jogar.UseVisualStyleBackColor = false;
+            this.btn_jogar.Click += new System.EventHandler(this.btn_jogar_Click);
+            // 
+            // btn_sair
+            // 
+            this.btn_sair.BackColor = System.Drawing.Color.DarkSalmon;
+            this.btn_sair.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.btn_sair.Location = new System.Drawing.Point(126, 252);
+            this.btn_sair.Name = "btn_sair";
+            this.btn_sair.Size = new System.Drawing.Size(90, 39);
+            this.btn_sair.TabIndex = 3;
+            this.btn_sair.Text = "Sair";
+            this.btn_sair.UseVisualStyleBackColor = false;
+            this.btn_sair.Click += new System.EventHandler(this.btn_sair_Click);
+            // 
+            // label2
+            // 
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.ForeColor = System.Drawing.Color.Blue;
+            this.label2.Location = new System.Drawing.Point(13, 307);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(315, 23);
+            this.label2.TabIndex = 5;
+            // 
+            // label3
+            // 
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 15.75F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label3.ForeColor = System.Drawing.Color.Blue;
+            this.label3.Location = new System.Drawing.Point(27, 28);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(291, 80);
+            this.label3.TabIndex = 6;
+            this.label3.Text = "Este programa gera um número entre 1 e 100. Tente advinhar.";
+            this.label3.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // textBox1
+            // 
+            this.textBox1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBox1.Location = new System.Drawing.Point(135, 130);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(70, 29);
+            this.textBox1.TabIndex = 7;
+            this.textBox1.KeyDown += new System.Windows.Forms.KeyEventHandler(this.textBox1_KeyDown);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.Cornsilk;
+            this.ClientSize = new System.Drawing.Size(340, 332);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.btn_sair);
+            this.Controls.Add(this.btn_jogar);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Jogo da advinhação";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button btn_jogar;
+        private System.Windows.Forms.Button btn_sair;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox1;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,77 @@
+﻿using System;
+using System.Windows.Forms;
+
+namespace Advinha_numero
+{
+    public partial class Form1 : Form
+    {
+        int minimo = 0; // variável que guarda o valor mínimo do número a ser gerado pelo computador
+        int maximo = 100; // variável que guarda o valor máximo do número a ser gerado pelo computador
+        int tentativas = 0; // número de tentativas até acertar o número
+        int numero = 0; // guarda o número aleatório gerado pelo computador
+
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            textBox1.Enabled = false; // desativa o textbox onde o usuário digita o número
+        }
+
+        private void btn_jogar_Click(object sender, EventArgs e)
+        {            
+            minimo = 0;  // numero mínimo para a geração
+            tentativas = 0; // contador de tentativas
+            Random rnd = new Random();
+            numero = rnd.Next(minimo, maximo); // guarda em numero o valor gerado entre 1 e 100
+            label2.Text = ""; // limpa o label de informações
+            label3.Text = "Jogo iniciado." + Environment.NewLine +
+                "Digite um número entre " + Environment.NewLine +
+                string.Format("{0} e {1}", minimo, maximo) + " e aperte Enter.";
+            textBox1.Enabled = true; // torna visível o textbox para digitar o numero
+            textBox1.Text = String.Empty;
+            textBox1.Select();  // coloca o foco no textbox1
+            btn_jogar.Visible = false;
+
+        }
+
+        private void btn_sair_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+
+        private void textBox1_KeyDown(object sender, KeyEventArgs e)
+        {
+            if (e.KeyCode == Keys.Return && textBox1.Text != "")
+            {
+                // transformação do texto em valor inteiro e colocando na variável valor
+                int.TryParse(textBox1.Text, out int valor);
+                if (valor < numero)  // se o valor digitado for menor que o numero gerado....
+                {
+                    label3.Text = "O número digitado é menor.";
+                    tentativas++;
+                    label2.Text = "Tentativas: " + tentativas.ToString();
+                }
+                else
+                if (valor > numero)  // se o valor digitado for maior que o numero gerado....
+                {
+                    label3.Text = "O número digitado é maior.";
+                    tentativas++;
+                    label2.Text = "Tentativas: " + tentativas.ToString();
+                }
+                else
+                {
+                    tentativas++;
+                    label2.Text = "Tentativas: " + tentativas.ToString();
+                    label3.Text = "Você acertou" + Environment.NewLine +
+                             " em " + tentativas + Environment.NewLine +
+                            "tentativas.";
+                    textBox1.Enabled = false;
+                    btn_jogar.Visible = true;
+                }
+            }
+        }
+    }
+}
